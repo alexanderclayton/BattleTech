aeab19f2d65ab252a6413f0511d64eb739d3fb92
Alex Clayton <alexander.clayton.email@gmail.com>
2023-07-25 18:50:50 -0500

    added mechs

--- src/helpers/mechspecs.ts
+++ src/helpers/mechspecs.ts
@@ -1,6 +1,12 @@
 import Flea from '../assets/LightMechImages/FleaCardImage.png'
+import Locust from '../assets/LightMechImages/LocustCardImage.png'
+import Commando from '../assets/LightMechImages/CommandoCardImage.png'
 import Cicada from '../assets/MediumMechImages/CicadaCardImage.png'
+import Assassin from '../assets/MediumMechImages/AssassinCardImage.png'
+import Hunchback from '../assets/MediumMechImages/HunchbackCardImage.png'
 import Dragon from '../assets/HeavyMechImages/DragonCardImage.png'
+import Warhammer from '../assets/HeavyMechImages/WarhammerCardImage.png'
+import BlackKnight from '../assets/HeavyMechImages/BlackKnightCardImage.png'
 import Atlas from '../assets/AssaultMechImages/AtlasCardImage.png'
 import Battlemaster from '../assets/AssaultMechImages/BattleMasterCardImage.png'
 import KingCrab from '../assets/AssaultMechImages/King_CrabCardImage.png'
@@ -23,16 +29,52 @@ export const MechsData: Mech[] = [
         image: Flea,
     },
     {
+        id: "Locust",
+        name: "Locust",
+        class: "Light",
+        image: Locust
+    },
+    {
+        id: "Commando",
+        name: "Commando",
+        class: "Light",
+        image: Commando
+    },
+    {
         id: "Cicada",
         name: "Cicada",
         class: "Medium",
         image: Cicada
     },
     {
+        id: "Assassin",
+        name: "Assassin",
+        class: "Medium",
+        image: Assassin
+    },
+    {
+        id: "Hunchback",
+        name: "Hunchback",
+        class: "Medium",
+        image: Hunchback
+    },
+    {
         id: "Dragon",
         name: "Dragon",
         class: "Heavy",
         image: Dragon
+    },
+    {
+        id: "Warhammer",
+        name: "Warhammer",
+        class: "Heavy",
+        image: Warhammer
+    },
+    {
+        id: "BlackKnight",
+        name: "Black Knight",
+        class: "Heavy",
+        image: BlackKnight
     },
     {
         id: "Atlas",
